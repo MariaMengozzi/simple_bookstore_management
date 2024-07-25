882a380066dc75b5778b21452b0ce7bbc8a12ffe
maria mengozzi <mengozzi.maria@gmail.com>
2024-07-25 09:38:00 +0200

    refactor: book repository test

--- src/test/kotlin/bookstore/management/repository/AllRepositoryTest.kt
+++ src/test/kotlin/bookstore/management/repository/AllRepositoryTest.kt
@@ -1,15 +1,18 @@
 package bookstore.management.repository
 
-
+import bookstore.management.entity.Author
 import bookstore.management.entity.Book
-import org.junit.jupiter.api.Assertions.*
-
+import bookstore.management.entity.Genre
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.ActiveProfiles
+import java.time.LocalDate
 import java.time.Year
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
 
 
 @DataJpaTest
@@ -17,16 +20,37 @@ import java.time.Year
 @ActiveProfiles("test")
 class BookRepositoryTest @Autowired constructor(
     val bookRepository: BookRepository,
+    val authorRepository: AuthorRepository,
+    val genreRepository: GenreRepository
 ){
     @Test
     fun `should save and retrieve a book by isbn`() {
+        val author = authorRepository.save(
+            Author(
+                name = "Arthur",
+                surname = "Conan Doyle",
+                birthdate = LocalDate.of(1859, 5, 22),
+                city = "Edinburgh",
+            )
+        )
+
+        val genres = setOf(
+                Genre(name = "Noir"),
+                Genre(name = "Thriller"),
+                Genre(name = "Mystery")
+            )
+
+        genreRepository.saveAll(genres)
+
 
         val book = bookRepository.save(
             Book(
                 isbn = "1405862335",
                 title = "A scandal in Bohemia",
                 price = 13.32,
-                publicationYear = Year.of(1891)
+                publicationYear = Year.of(1891),
+                authors = setOf(author),
+                genres = genres
             )
         )
 
@@ -37,6 +61,31 @@ class BookRepositoryTest @Autowired constructor(
 
     @Test
     fun `should retrieve books by title`() {
+        val author1 = authorRepository.save(
+            Author(
+                name = "a",
+                surname = "1",
+                birthdate = LocalDate.of(1859, 5, 22),
+                city = "Edinburgh",
+            )
+        )
+
+        val author2 = authorRepository.save(
+            Author(
+                name = "a",
+                surname = "2",
+                birthdate = LocalDate.of(1859, 5, 22),
+                city = "Edinburgh",
+            )
+        )
+
+        val genres = setOf(
+            Genre(name = "Noir"),
+            Genre(name = "Thriller"),
+            Genre(name = "Mystery")
+        )
+
+        genreRepository.saveAll(genres)
 
 
         val book1 = bookRepository.save(
@@ -45,6 +94,8 @@ class BookRepositoryTest @Autowired constructor(
                 title = "book",
                 price = 13.32,
                 publicationYear = Year.of(1891),
+                authors = setOf(author1),
+                genres = genres
             )
         )
 
@@ -54,6 +105,8 @@ class BookRepositoryTest @Autowired constructor(
                 title = "book",
                 price = 13.32,
                 publicationYear = Year.of(1891),
+                authors = setOf(author2),
+                genres = genres
             )
         )
 

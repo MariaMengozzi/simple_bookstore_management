202036fdb5be130756c861fd682713168a973e82
maria mengozzi <mengozzi.maria@gmail.com>
2024-07-25 08:40:45 +0200

    ci: remove mysql

--- src/test/kotlin/bookstore/management/TestManagementApplication.kt
+++
@@ -1,9 +0,0 @@
-package bookstore.management
-
-import org.springframework.boot.fromApplication
-import org.springframework.boot.with
-
-
-fun main(args: Array<String>) {
-	fromApplication<ManagementApplication>().with(TestcontainersConfiguration::class).run(*args)
-}

622da862266224b072c639446edf6716353174f5
maria mengozzi <mengozzi.maria@gmail.com>
2024-07-31 14:13:28 +0200

    refactor: exception controller

--- src/main/kotlin/bookstore/management/controller/AuthorController.kt
+++ src/main/kotlin/bookstore/management/controller/AuthorController.kt
@@ -3,19 +3,11 @@ package bookstore.management.controller
 import bookstore.management.entity.Author
 import bookstore.management.service.AuthorService
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/author")
-class AuthorController (private val service: AuthorService) {
-    @ExceptionHandler(NoSuchElementException::class)
-    fun handleNotFound(e: NoSuchElementException): ResponseEntity<String> =
-        ResponseEntity(e.message, HttpStatus.NOT_FOUND)
-
-    @ExceptionHandler(IllegalArgumentException::class)
-    fun handleBadRequest(e: IllegalArgumentException): ResponseEntity<String> =
-        ResponseEntity(e.message, HttpStatus.BAD_REQUEST)
+class AuthorController (private val service: AuthorService) : ExceptionController() {
 
     @GetMapping
     fun getAuthors(): Collection<Author> = service.getAllAuthors()
